7aebd725e99dfc53ea40692b393351d61afb82b7
CarlosHyperland <juegapilollino@gmail.com>
2026-02-22 12:04:12 -0600

    Primer commit

---
+++ Persona.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class Persona{
+	//atributos
+	protected : string cui, nombres, apellidos,direccion,fecha_nacimiento;
+		  int telefono;
+		  bool genero;
+		  
+	//contructor
+	protected :
+		  Persona(){
+		  }
+		  Persona(string cu, string nom, string ape, string dir, string fech,int tel,bool gen){
+			cui = cu;
+			nombres = nom;
+			apellidos, ape;
+			direccion = dir;
+			telefono = tel;
+			fecha_nacimiento; fech;
+			genero = gen;
+
+		  }
+	//metodo (CRUD)
+	void crea();
+	void leer();
+	void actulizar();
+	void borrar();
+
+};
