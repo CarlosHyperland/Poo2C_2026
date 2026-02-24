3cc1897f831477e90f2e4a77cedd33c60ac0c652
CarlosHyperland <juegapilollino@gmail.com>
2026-02-24 01:42:17 -0600

    actulizado3

--- Estudiante.cpp
+++ Estudiante.cpp
@@ -1,8 +1,9 @@
-#iostream <<Persona.cpp>>
-#iostream <<iostream>>
+#include <iostream>
+#include "Persona.cpp"
+
 using namespace std;
 
-class Estudiante: Persona{
+class Estudiante: Public Persona{
 	//atributos
 	private : string carnet;
 
@@ -11,28 +12,14 @@ class Estudiante: Persona{
 
 	Estudiante(){
 	}
-	Estudiante(string cu,string nom,string ape,string dir,string fech,int tel,bool gen,string car): Persona(cu,nom,ape,dir,fech,tel,gen,car){
+	Estudiante(string cu,string nom,string ape,string dir,string fech,int tel,bool gen,string car): Persona(cu,nom,ape,dir,fech,tel,gen){
 	  carnet = car;
 	}
 	//metodos
 	//set (modificar)
-	void setCui(string cu){cui = cu;}
-	void setNombres(string nom){nombre = nom;}
-	void setCui(string ape){apellido = ape;}
-	void setCui(string dir){direccion = dir;}
-	void setCui(string fech){fecha_nacimiento = fech;}
-	void setCui(int tel){telefono = tel;}
-	void setCui(bool gen){genero = gen;}
-	void setCui(string car){carnet = car;}
+	void setCarnet(string car){carnet = car;}
 
 	//get (mostrar)
-	string getCui(){return cui;}
-	string getNombres(){return nombres;}
-	string getApellidos(){return apellidos;}
-	string getDireccion(){return direccion;}
-	string getFecha_nacimiento(){return fecha_nacimiento;}
-	int gettelefono(){return telefono;}
-	bool getGenero(){return genero;}
 	string getCarnet(){return carnet;}
 	//metodos
 	void mostrar(){
